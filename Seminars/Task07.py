a3bd0f402ea3606b0d250791873e3663ae968974
artem.chernomazov <pechenkazz@mail.ru>
2022-10-12 05:11:52 +0300

    complete tasks

---
+++ Seminars/Task07.py
@@ -0,0 +1,11 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# ¬ - not
+# ⋁ - or
+# ⋀ - and
+# not(x or y or z) = not x and not y and not z
+
+for x in (0,1):
+    for y in (0,1):
+        for z in (0,1):
+            print( x,y,z, end= ' : ')
+            print(not(x or y or z) == ((not x) and (not y) and (not z)))
