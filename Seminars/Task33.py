444377a511d0b945cabc51fde23fb8c93fc583a6
artem.chernomazov <pechenkazz@mail.ru>
2022-10-23 15:49:43 +0300

    hometask4

--- Seminars/Task33.py
+++ Seminars/Task33.py
@@ -6,13 +6,13 @@ from random import randint
 from sympy import symbols
 from math import prod
 
-max_val = 100
+max_value = 100
 k = int(input('Введите натуральную степень k:'))
 
-koeff = [randint(-max_val, max_val) for i in range(k)] + [randint(1, max_val)]
+coefficient = [randint(-max_value, max_value) for i in range(k)] + [randint(1, max_value)]
 x = symbols('x')
 
-print(sum(map(prod, zip(koeff, [ x**i for i in range(k + 1)]))), '= 0')
+print(sum(map(prod, zip(coefficient, [ x**i for i in range(k + 1)]))), '= 0')
 
-with open('file.txt','w', encoding='utf-8') as file:
-    file.write(str(sum(map(prod, zip(koeff, [x ** i for i in range(k + 1)])))) + '= 0')
+with open('file2.txt','w', encoding='utf-8') as file:
+    file.write(str(sum(map(prod, zip(coefficient, [x ** i for i in range(k + 1)])))) + '= 0')
